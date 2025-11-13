8e4d3680df56cefc5c219fc2bdd95e4e9ee72c0a
Anthony Ilori <67214076+anth-05@users.noreply.github.com>
2025-11-13 18:54:37 +0100

    updated to use new database

---
+++ config.js
@@ -0,0 +1,5 @@
+// config.js
+export const API_BASE_URL =
+  window.location.hostname === "localhost"
+    ? "http://localhost:5000"
+    : "https://your-backend-name.onrender.com"; // 🔁 change this URL
