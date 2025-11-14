cfa525263839ab0f2829bc5df9d40fc46a27caea
Anthony Ilori <67214076+anth-05@users.noreply.github.com>
2025-11-14 12:38:39 +0100

    moved files again

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,20 @@ import multer from "multer";
 import http from "http";
 import { Server } from "socket.io";
 
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Serve frontend
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+
 const { Pool } = pkg;
 
 dotenv.config();
